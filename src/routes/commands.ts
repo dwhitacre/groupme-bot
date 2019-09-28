becfa3576b79914f50366325bf75c64f132e660f
Dan Whitacre <whitacre.74@osu.edu>
2019-09-28 16:53:24 -0400

    add home page, commands api

---
+++ src/routes/commands.ts
@@ -0,0 +1,43 @@
+import { Server } from '@hapi/hapi'
+
+import getCommands from '../commands'
+
+export default function register(server: Server): void {
+  server.route({
+    method: 'GET',
+    path: '/commands',
+    options: {
+      handler: async (): Promise<{
+        [propName: string]: {
+          desc: string
+          usage: string
+          command: string
+        }
+      }> => {
+        const commands = await getCommands(server)
+        const commandsFiltered: {
+          [propName: string]: {
+            desc: string
+            usage: string
+            command: string
+          }
+        } = {}
+
+        for (const commandName in commands) {
+          if (commands.hasOwnProperty(commandName)) {
+            commandsFiltered[commandName] = {
+              desc: commands[commandName].desc,
+              usage: commands[commandName].usage,
+              command: commandName,
+            }
+          }
+        }
+
+        return commandsFiltered
+      },
+      description: 'The commands in the bot.',
+      notes: 'GET the commands in the bot.',
+      tags: ['api', 'commands'],
+    },
+  })
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Server } from '@hapi/hapi'
 
+import commands from './commands'
 import health from './health'
+import home from './home'
 import hooks from './hooks'
 
 export default function register(server: Server): void {
+  commands(server)
   health(server)
   hooks(server)
+  home(server)
 }
